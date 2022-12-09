72f0f5e8a4eeedf406c05b658c908b75f79734bd
behbud <behbudez@code.edu.az>
2022-12-10 00:09:56 +0400

    Add project files.

---
+++ Homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eded = 400;
+            int faiz = 20;
+            int ededinFaizi = eded * faiz / 100;
+            Console.WriteLine(ededinFaizi);
+
+            Console.ReadKey();
+        }
+    }
+}
